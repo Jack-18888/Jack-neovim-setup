f2f4649e9007d1206de019b8794ce8492c6d9b76
Jack-18888 <jack.shi.smart@gmail.com>
2026-03-07 01:28:04 -0500

    enabled github copilot

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -2,23 +2,23 @@
 
 return {
   -- Main Copilot plugin
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   cmd = 'Copilot',
-  --   event = 'InsertEnter', -- or 'BufEnter'
-  --   config = function()
-  --     require('copilot').setup {
-  --       suggestion = {
-  --         enabled = true,
-  --         auto_trigger = true,
-  --         -- More configuration options here
-  --       },
-  --       panel = {
-  --         enabled = false, -- Set to true for a separate window
-  --       },
-  --       -- ... other settings
-  --     }
-  --   end,
-  -- },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter', -- or 'BufEnter'
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = false,
+          auto_trigger = true,
+          -- More configuration options here
+        },
+        panel = {
+          enabled = false, -- Set to true for a separate window
+        },
+        -- ... other settings
+      }
+    end,
+  },
 }
 
