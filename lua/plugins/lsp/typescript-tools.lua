ac0555be259942700b548a4fd23c6541c0017609
Jack-18888 <jack.shi.smart@gmail.com>
2026-05-08 14:50:08 -0400

    sync typescript-tools with lsp on_attach keymaps

--- lua/plugins/lsp/typescript-tools.lua
+++ lua/plugins/lsp/typescript-tools.lua
@@ -4,6 +4,32 @@ return {
     "pmizio/typescript-tools.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  }
+    config = function()
+      -- 1. Define the on_attach function with your keymaps
+      local on_attach = function(client, bufnr)
+        local nmap = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = bufnr, noremap = true, silent = true, desc = "LSP: " .. desc })
+        end
+
+        nmap("gd", vim.lsp.buf.definition, "Go to Definition")
+        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+        nmap("gi", vim.lsp.buf.implementation, "Go to Implementation")
+        nmap("gr", vim.lsp.buf.references, "Find References")
+        nmap("<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+        nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+        nmap("<leader>e", vim.diagnostic.open_float, "Show Diagnostics")
+      end
+
+      -- 2. Setup the plugin with the on_attach function
+      require("typescript-tools").setup({
+        on_attach = on_attach,
+        settings = {
+          -- spawn additional tsserver processes for faster response
+          separate_diagnostic_server = true,
+          -- publish diagnostics on typing
+          publish_diagnostic_on = "insert_leave",
+        },
+      })
+    end,
+  },
 }
