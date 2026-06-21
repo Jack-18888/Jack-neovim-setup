0c0bdd1697ddf6c797cb7b152cd0e9f800ff1b42
Jack-18888 <jack.shi.smart@gmail.com>
2026-06-21 17:44:53 -0400

    fix nvim-tree buffer closing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,27 +4,56 @@
 vim.keymap.set({ "i", "n" }, "<C-s>", "<cmd>w<CR>", { noremap = true, silent = true, desc = "Save file" })
 vim.keymap.set({ "i", "n" }, "<C-p>", "<cmd>80vsp | term pwsh<CR>", { noremap = true, silent = true, desc = "Open terminal in split" })
 
-vim.keymap.set("n", "<C-a>", "gg0vG$", { noremap = true, silent = true, desc = "Select all" })
-vim.keymap.set("n", "<leader>q", function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  
-  -- Get a list of all listed buffers (excluding neo-tree)
-  local buffers = vim.tbl_filter(function(b)
-    return vim.api.nvim_buf_is_valid(b) and vim.bo[b].buflisted
-  end, vim.api.nvim_list_bufs())
+local function safe_close_buffer(force)
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_win = vim.api.nvim_get_current_win()
 
-  -- If we are closing the last buffer, create a new empty one first
-  if #buffers <= 1 then
-    vim.cmd("enew")
-  else
-    -- Otherwise, go to the previous buffer before deleting
-    vim.cmd("bp")
+  if vim.bo[current_buf].filetype == "NvimTree" then
+    return
   end
 
-  -- Delete the original buffer
-  vim.cmd("bd " .. bufnr)
+  local listed_buffers = {}
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buflisted and vim.bo[bufnr].filetype ~= "NvimTree" then
+      table.insert(listed_buffers, bufnr)
+    end
+  end
+
+  local target_buf
+  if #listed_buffers <= 1 then
+    vim.cmd("enew")
+  else
+    for index, bufnr in ipairs(listed_buffers) do
+      if bufnr == current_buf then
+        target_buf = listed_buffers[index - 1] or listed_buffers[index + 1]
+        break
+      end
+    end
+
+    if target_buf and target_buf ~= current_buf then
+      vim.api.nvim_win_set_buf(current_win, target_buf)
+    end
+  end
+
+  if vim.api.nvim_buf_is_valid(current_buf) then
+    vim.api.nvim_buf_delete(current_buf, { force = force })
+  end
+end
+
+vim.api.nvim_create_user_command("Bd", function(opts)
+  safe_close_buffer(opts.bang)
+end, { bang = true })
+
+vim.cmd([[cnoreabbrev bd Bd]])
+vim.cmd([[cnoreabbrev bd! Bd!]])
+
+vim.keymap.set("n", "<C-a>", "gg0vG$", { noremap = true, silent = true, desc = "Select all" })
+vim.keymap.set("n", "<leader>q", function()
+  safe_close_buffer(false)
 end, { noremap = true, silent = true, desc = "Close buffer and split" })
-vim.keymap.set("n", "<leader>fq", ":bd!<CR>", { noremap = true, silent = true, desc = "Force close buffer and split" })
+vim.keymap.set("n", "<leader>fq", function()
+  safe_close_buffer(true)
+end, { noremap = true, silent = true, desc = "Force close buffer and split" })
 
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file tree" })
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
